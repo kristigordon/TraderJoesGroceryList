a1c0b20e2ddb4ab09ae5856069d28fb5cd7a4554
Kristi Gordon <66803124+kristigordon@users.noreply.github.com>
2023-01-22 15:30:59 -0500

    Upload code files

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// var button = document.getElementsByTagName("button") [0];
+
+// button.addEventListener("click", function() {
+// 	console.log("CLICK!");
+// })
+
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
+var remove = document.getElementById("remove");
 var ul = document.querySelector("ul");
 
 function inputLength() {
@@ -9,8 +16,13 @@ function inputLength() {
 function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
+	ul.appendChild(li);	
 	input.value = "";
+}
+
+function removeAfterClick() {
+	console.log("Remove");
+	remove.done
 }
 
 function addListAfterClick() {
@@ -28,3 +40,7 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+remove.addEventListener("click", removeAfterClick);
+
+
